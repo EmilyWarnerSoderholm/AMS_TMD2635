b2e5e626e2d63d96caf3af8b8d8febd7646caa85
EmilyWarnerSoderholm <60695881+EmilyWarnerSoderholm@users.noreply.github.com>
2021-04-22 11:26:03 +0200

    Update TMD2635.h

--- src/TMD2635.h
+++ src/TMD2635.h
@@ -29,6 +29,11 @@ LICENSE:
 #include <Wire.h>
 #include <Arduino.h>
 
+#define SCL     4 // I2C bus
+#define SDA     5 //
+#define SCL_IN  RB4    //
+#define SDA_IN  RB1    //
+ 
 
 class TMD2635
 {
@@ -43,57 +48,72 @@ class TMD2635
     enum regAddr
     {
 {
-    DEVREG_ENABLE       =   0x80, /**  ENABLE R/W Enables states and interrupts. Reset val 0x00 */
+    /////// Specified for near proximity (NP)! ////
+    
+    ENABLE       =   0x80, /**  ENABLE R/W Enables states and interrupts. Reset val 0x00 */
 
-    DEVREG_ATIME        =   0x81, /**  ATIME R/W ADC integration time 0x00 */
-    DEVREG_PTIME        =   0x82, /**  PRATE R/W Proximity time 0x1F */
-    DEVREG_PRATE        =   DEVREG_PTIME,
+    ATIME        =   0x81, /**  ATIME R/W ADC integration time 0x00 */
+    PTIME        =   0x82, /**  PRATE R/W Proximity time 0x1F */
+    PRATE        =   PTIME,
 
     /** PROX related interrupt thresholds */
-    DEVREG_PILTL        =   0x88, /**  PILTL R/W Proximity interrupt low threshold low byte 0x00 */
-    DEVREG_PILTH        =   0x89, /**  PILTH R/W Proximity interrupt low threshold high byte 0x00 */
-    DEVREG_PIHTL        =   0x8A, /**  PIHTL R/W Proximity interrupt high threshold low byte 0x00 */
-    DEVREG_PIHTH        =   0x8B, /**  PIHTH R/W Proximity interrupt high threshold high byte 0x00 */
- 
-    DEVREG_PERS         =   0x8C, /**  PERS R/W ALS and proximity interrupt persistence filters 0x00 */
+    PILTL        =   0x88, /**  PILTL R/W Proximity interrupt low threshold low byte 0x00 */
+    PILTH        =   0x89, /**  PILTH R/W Proximity interrupt low threshold high byte 0x00 */
+        // NP: IGNORE
+    PIHTL        =   0x8A, /**  PIHTL R/W Proximity interrupt high threshold low byte 0x00 */
+        // NP: contains the HIGH threshold (8-bit value)
+    PIHTH        =   0x8B, /**  PIHTH R/W Proximity interrupt high threshold high byte 0x00 */
+        // NP: IGNORE
+        
+    PERS         =   0x8C, /**  PERS R/W ALS and proximity interrupt persistence filters 0x00 */
 
-    DEVREG_CFG0         =   0x8D, /**  CFG0 R/W Configuration zero 0x40 */
-    DEVREG_PROX_CFG0    =   0x8E, /**  PCFG0 R/W Proximity configuration zero 0x4F */
-    DEVREG_PROX_CFG1    =   0x8F, /**  PCFG1 R/W Proximity configuration one 0x80 */
-    DEVREG_CFG1         =   0x90, /**  CFG1 R/W Configuration one 0x09 */
-    DEVREG_REV_ID_1     =   0x91, /**  REVID R Revision ID 0x01 */
-    DEVREG_ID           =   0x92, /**  ID R Device ID 0x10 */
+    CFG0         =   0x8D, /**  CFG0 R/W Configuration zero 0x40 */
+    PROX_CFG0    =   0x8E, /**  PCFG0 R/W Proximity configuration zero 0x4F */
+        // NP: PGAIN (7:6) = 1 (00)
+        // NP: PPULSE(5:0) = 5 (00101) PPULSE determines no of prox pulses to be transmitted
+    PROX_CFG1    =   0x8F, /**  PCFG1 R/W Proximity configuration one 0x80 */
+        // NP: PLDRIVE (3:0) = 7 (0111)
+        // NP: PPULSE_LEN (3:0) = 12 (100)
+    CFG1         =   0x90, /**  CFG1 R/W Configuration one 0x09 */
+    REV_ID_1     =   0x91, /**  REVID R Revision ID 0x01 */
+    ID           =   0x92, /**  ID R Device ID 0x10 */
 
-    DEVREG_STATUS       =   0x9B, /**  STATUS R, SC Device status 0x00 */
+    STATUS       =   0x9B, /**  STATUS R, SC Device status 0x00 */
 
-    /** PROX related raw data counts */
-    DEVREG_PDATA_LOW    =   0x9C, /**  PDATAL R Proximity ADC low data 0x00 */
-    DEVREG_PDATA_HI     =   0x9D, /**  PDATAH R Proximity ADC high data 0x00 */
-    DEVREG_PDATA_START  =   DEVREG_PDATA_LOW,
+    /** PROX related raw data counts PDATA= ADCval x 16/PPULSE, */
+        // NP: APC is disabled, only the upper 8 bits are compared against the
+        // threshold values contained in PILTL and PIHTL.
+    PDATAL    =   0x9C, /**  PDATAL R Proximity ADC low data (8 bits) 0x00 */
+    PDATAH     =   0x9D, /**  PDATAH R Proximity ADC high data (2 bits) 0x00 */
+        //  NP: bits 1:0 contain the lower 2 bits of the 10-bit proximity value.
+    PDATA_START  =   DEVREG_PDATA_LOW,
 
-    DEVREG_REV_ID_2     =   0xA6, /**  REVID2 R Revision ID two 0x05 */
-    DEVREG_CFG2         =   0xA7, /**  CFG2 R/W Configuration two 0x00 */
-    DEVREG_SOFT_RESET   =   0xA8, /**  SOFTRST R/W Soft reset 0x00 */
-    DEVREG_PWTIME       =   0xA9, /**  SOFTRST R/W Soft reset 0x00 */
-    DEVREG_CFG8         =   0xAA, /**  CFG8 R/W Configuration three 0x04 */
-    DEVREG_CFG3         =   0xAB, /**  CFG3 R/W Configuration three 0x04 */
+    REV_ID_2     =   0xA6, /**  REVID2 R Revision ID two 0x05 */
+    CFG2         =   0xA7, /**  CFG2 R/W Configuration two 0x00 */
+    SOFT_RESET   =   0xA8, /**  SOFTRST R/W Soft reset 0x00 */
+    PWTIME       =   0xA9, /**  SOFTRST R/W Soft reset 0x00 */
+    CFG8         =   0xAA, /**  CFG8 R/W Configuration three 0x04 */
+    CFG3         =   0xAB, /**  CFG3 R/W Configuration three 0x04 */
 //    DEVREG_CFG4         =   0xAC, /**  CFG4 R/W Configuration four (must be set to 0x3D) 0x3F */
-    DEVREG_CFG6         =   0xAE, /**  CFG6 R?W Configuration six 0x3F */
+    CFG6         =   0xAE, /**  CFG6 R?W Configuration six 0x3F */
+        // NP: APC = disabled by setting bit 6 = 1,
 
-    DEVREG_PFILTER      =   0xB3, /**  PFILTER */
+    PFILTER      =   0xB3, /**  PFILTER */
 
-    DEVREG_POFFSET_L    =   0xC0, /**  POFFSETL R/W Proximity offset low data 0x00 */
-    DEVREG_POFFSET_H    =   0xC1, /**  POFFSETH R/W Proximity offset high data 0x00 */
+    POFFSET_L    =   0xC0, /**  POFFSETL R/W Proximity offset low data 0x00 */
+    POFFSET_H    =   0xC1, /**  POFFSETH R/W Proximity offset high data 0x00 */
 
     /** Calibration relates registers */
-    DEVREG_CALIB        =   0xD7, /**  CALIB R/W Proximity offset calibration 0x00 */
-    DEVREG_CALIBCFG     =   0xD9, /**  CALIBCFG R/W Proximity offset calibration control 0x50 */
-    DEVREG_CALIBSTAT    =   0xDC, /**  CALIBSTAT R Proximity offset calibration status 0x00 */
-    DEVREG_INTENAB      =   0xDD, /**  INTENAB R/W Interrupt enables 0x00 */
+    CALIB        =   0xD7, /**  CALIB R/W Proximity offset calibration 0x00 */
+    CALIBCFG     =   0xD9, /**  CALIBCFG R/W Proximity offset calibration control 0x50 */
+        //BINSRCH_TARGET(7:5) = 31 (011)
+    CALIBSTAT    =   0xDC, /**  CALIBSTAT R Proximity offset calibration status 0x00 */
+    INTENAB      =   0xDD, /**  INTENAB R/W Interrupt enables 0x00 */
 
     /** Factory data lot codes */
-    DEVREG_FACT_CODE_L  =   0xE5, /**  FAC_L R Factory data low (lot code data) 0x00 to 0xFF */
-    DEVREG_FACT_CODE_H  =   0xE6, /**  FAC_H R Factory data high (lot code data) 0x00 to 0xFF */
+    FACT_CODE_L  =   0xE5, /**  FAC_L R Factory data low (lot code data) 0x00 to 0xFF */
+    FACT_CODE_H  =   0xE6, /**  FAC_H R Factory data high (lot code data) 0x00 to 0xFF */
+    TEST9        =   0x07, /** Must be set to this */
 
     };
   private:
